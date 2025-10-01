6fcc4fdc071a042b8eb07637422fad03c3947be8
Naga Lalitha Sree Thatavarthi <thatavarthinagalalithasree2020@gmail.com>
2025-09-30 21:31:25 -0400

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: Annika Lisa Lang
-# DATE CREATED: 18.06.2020                                  
-# REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
